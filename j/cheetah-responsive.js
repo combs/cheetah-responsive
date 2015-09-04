443166cb6bfd4d0f06e9533701d15bda6eb7cdc2
combs <ccombs@gmail.com>
2015-09-04 12:52:13 -0400

    Update ad tags to increment the ID.

--- j/cheetah-responsive.js
+++ j/cheetah-responsive.js
@@ -1,20 +1,4 @@
 
-/* Ad tools. */
-
-var googletag = googletag || {};
-googletag.cmd = googletag.cmd || [];
-(function() {
-  var gads = document.createElement('script');
-  gads.async = true;
-  gads.type = 'text/javascript';
-  var useSSL = 'https:' == document.location.protocol;
-  gads.src = (useSSL ? 'https:' : 'http:') +
-    '//www.googletagservices.com/tag/js/gpt.js';
-  var node = document.getElementsByTagName('script')[0];
-  node.parentNode.insertBefore(gads, node);
-})();
-  
-  
 
 
 
@@ -22,41 +6,50 @@ googletag.cmd = googletag.cmd || [];
 
 function prepareAds() {
 
+	var theIndex=0;
+	
 	$(".ad-unit-728").each( function() {
 		var theID=$(this).attr("id");
 		if (theID) {
-		  googletag.cmd.push(function() {
-    		googletag.defineSlot('/2994/ng.ngm/climate-change', [[728, 90], [320, 50]], theID ).addService(googletag.pubads());
-		    googletag.pubads().enableSingleRequest();
-		    googletag.enableServices();
-		    googletag.display(theID);
-		  });
+			theIndex++;
+			googletag.cmd.push(function() {
+	    		googletag.defineSlot("/2994/ng.ngm/" + ( theIndex>1 ? "ng" + theIndex + "_" : "") + "climate-change", [[728, 90], [320, 50]], theID ).addService(googletag.pubads());
+			    googletag.pubads().enableSingleRequest();
+			    googletag.enableServices();
+			    googletag.display(theID);
+		  	});
 		}
 		
 	});
+	
+	theIndex=0;
 	
 	$(".ad-unit-210").each( function() {
 		var theID=$(this).attr("id");
 		if (theID) {
-		  googletag.cmd.push(function() {
-    		googletag.defineSlot('/2994/ng.ngm/climate-change', [210, 50], theID ).addService(googletag.pubads());
-		    googletag.pubads().enableSingleRequest();
-		    googletag.enableServices();
-		    googletag.display(theID);
-		  });
+			theIndex++;
+			  googletag.cmd.push(function() {
+	    		googletag.defineSlot("/2994/ng.ngm/" + ( theIndex>1 ? "ng" + theIndex + "_" : "") + "climate-change", [210, 50], theID ).addService(googletag.pubads());
+			    googletag.pubads().enableSingleRequest();
+			    googletag.enableServices();
+			    googletag.display(theID);
+			  });
 		}
 		
 	});
 	
+	theIndex=0;
+	
 	$(".ad-unit-300").each( function() {
 		var theID=$(this).attr("id");
 		if (theID) {
-		  googletag.cmd.push(function() {
-    		googletag.defineSlot('/2994/ng.ngm/climate-change', [300, 250], theID ).addService(googletag.pubads());
-		    googletag.pubads().enableSingleRequest();
-		    googletag.enableServices();
-		    googletag.display(theID);
-		  });
+			theIndex++;
+			  googletag.cmd.push(function() {
+	    		googletag.defineSlot("/2994/ng.ngm/" + ( theIndex>1 ? "ng" + theIndex + "_" : "") + "climate-change", [300, 250], theID ).addService(googletag.pubads());
+			    googletag.pubads().enableSingleRequest();
+			    googletag.enableServices();
+			    googletag.display(theID);
+			  });
 		}
 		
 	});
@@ -100,7 +93,23 @@ function prepareIncludes() {
 }
 
 $(document).ready(function() {
+		
+	/* Ad tools. */
 	
+	var googletag = googletag || {};
+	googletag.cmd = googletag.cmd || [];
+	(function() {
+	  var gads = document.createElement('script');
+	  gads.async = true;
+	  gads.type = 'text/javascript';
+	  var useSSL = 'https:' == document.location.protocol;
+	  gads.src = (useSSL ? 'https:' : 'http:') +
+	    '//www.googletagservices.com/tag/js/gpt.js';
+	  var node = document.getElementsByTagName('script')[0];
+	  node.parentNode.insertBefore(gads, node);
+	})();
+  
+  
 	prepareIncludes();
 	prepareDrawers();
 	prepareAds();
